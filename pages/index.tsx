f3158239eea1b2ab19356d7a3d898a35ab90194d
Luis Felipe <luisfelipesdn12@gmail.com>
2021-11-02 13:41:12 -0300

    back to styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-dom-confetti';
 import styled from 'styled-components'
 
 const Title = styled.h1`
@@ -6,5 +8,31 @@ const Title = styled.h1`
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowConfetti(true);
+    }, 1000);
+  }, []);
+
+  const config = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 150,
+    dragFriction: 0.12,
+    duration: 10000,
+    stagger: 3,
+    width: "1rem",
+    height: "1rem",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a", "#6247aa", "#815ac0", "#9163cb", "#a06cd5", "#b185db", "#c19ee0", "#d2b7e5", "#e2cfea"],
+  };
+
+  return (
+    <>
+      <Confetti active={showConfetti} config={config} />
+      <Title>My page</Title>
+    </>
+  );
 }
